28320f5094cd4e3a5bd8c6d24b4db84e8e1eb87f
Douglas Andrade <douglas.andrade@icarotech.com>
2022-01-25 16:46:15 -0300

    change credentials

--- src/client/elasticSearch.js
+++ src/client/elasticSearch.js
@@ -4,11 +4,11 @@ function getElasticSearchClient() {
 
     return new Client({
         cloud: {
-            id: 'Equinix:c291dGhhbWVyaWNhLWVhc3QxLmdjcC5lbGFzdGljLWNsb3VkLmNvbSRkZjg5NzZhYjhkZmE0YjVjOWVjYmQ2NzNmOTFkZWRkNSQwNWViNzE0NDA1YjY0ZWNkODgwNWExNGE5NmZhYTkwMQ=='
+            id: 'YourCloudID'
         },
         auth: {
             username: 'elastic',
-            password: 'GJ4MwTNJevxVtaef6sDVqIEE'
+            password: 'YourPassword'
         }
     });
     
